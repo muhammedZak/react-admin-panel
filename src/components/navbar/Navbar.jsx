191377d83abed6d07443fd55c13e72e366834bf9
muhammedZak <zakariya.zaku018@gmail.com>
2024-01-29 23:17:35 +0530

    Added navigation bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,53 @@
 import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search here" />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import BadgeIcon from '@mui/icons-material/Badge';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
@@ -6,23 +18,69 @@ const Sidebar = () => {
       <div className="top">
         <span className="logo">Admin</span>
       </div>
+      <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ProductionQuantityLimitsIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ViewModuleIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <BadgeIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <QueryStatsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+      </div>
     </div>
   );
 };
